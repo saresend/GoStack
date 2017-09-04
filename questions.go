0978db29361cd1796963513531187b8ecbea21c9
Samuel Resendez <saresend@usc.edu>
2017-09-03 23:04:10 -0700

    implemented retrieving questions from the API

--- questions.go
+++ questions.go
@@ -1,1 +1,19 @@
 package GoStack
+
+import "net/http"
+import "io/ioutil"
+import "encoding/json"
+
+//GetQuestions gets all the questions from a given site
+func GetQuestions(site string) []Question {
+	resp, err := http.Get("https://api.stackexchange.com/2.2/questions?site=" + site)
+	if err != nil {
+		return []Question{}
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	var questionResponse QuestionResponse
+	json.Unmarshal(body, &questionResponse)
+
+	return questionResponse.Items
+}
