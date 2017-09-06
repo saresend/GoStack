957d6d457c04b879ae723d5a731cb2b149894286
Samuel Resendez <saresend@usc.edu>
2017-09-05 19:58:37 -0700

    added Search Functionality

--- search.go
+++ search.go
@@ -1,5 +1,35 @@
 package GoStack
 
-func SearchStackExchange(keywords []string) {
+import "net/http"
+import "io/ioutil"
+import "encoding/json"
 
+//SearchStackExchange calls the search Functionality of stackexchange
+func SearchStackExchange(keywords []string, site string) ([]SearchItem, error) {
+	url := "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&site=" + site + "&tagged=" + addParams(keywords)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var response SearchResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	return response.Items, nil
+}
+
+func addParams(keywords []string) string {
+	str := ""
+	for i := 0; i < len(keywords); i++ {
+		str += keywords[i]
+		if i+1 < len(keywords) {
+			str += "%3B"
+		}
+	}
+	return str
 }
